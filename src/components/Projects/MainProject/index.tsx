9b02a646eda98648ba3ed9875d92b275f961713d
Roger2405 <rogerdr.rdr@gmail.com>
2023-02-23 17:08:47 -0300

    updating project AjudaME

--- src/components/Projects/MainProject/index.tsx
+++ src/components/Projects/MainProject/index.tsx
@@ -27,8 +27,8 @@ export default function MainProject() {
         <section className='mainProject'>
             <div className='header'>
                 <div className='header__info'>
-                    <img src={AjudameIcon} />
-                    <div>
+                    <img src="https://play-lh.googleusercontent.com/POOxI0XgrcFa1slX4-yu1vBDwN_EaPL_c3w-tXqgYOl35Q2_yjQIvCL2iXzC9CeAbx40=w240-h480-rw" />
+                    <div className='header__info--name'>
                         <h3>{mainProject.name}</h3>
                         <h4>{mainProject.subtitle}</h4>
                     </div>
@@ -58,17 +58,17 @@ export default function MainProject() {
                         Com a interface simples e intuitiva, o projeto busca uma inclusão digital para Micro Empreendedores Individuais que ainda não possuem um controle formal e eficaz do seu negócio, ou, até mesmo, empreendedores informais que queiram alavancar o seu negócio e, posteriormente, formalizá-lo.
                     </p>
                     <p className=''>
-                        No aplicativo será possível obter o histórico de vendas, visualizar os produtos mais vendidos, além de consultar gráficos, facilitando a interpretação dos dados e, consequentemente, proporcionando um maior controle sobre suas vendas.
+                        No aplicativo será possível obter o histórico de vendas, visualizar os produtos mais vendidos, além de consultar gráficos, facilitando a interpretação dos dados e, consequentemente, proporcionando um maior controle sobre seu negócio.
                     </p>
                 </div>
 
                 <p>
-                    Este é um projeto pessoal que também está
-                    sendo utilizado como trabalho acadêmico do curso
-                    de ADS.
+                    Este é um projeto pessoal que também foi utilizado como trabalho acadêmico do curso de ADS.
                 </p>
                 <div className='div-link'>
-                    <LinkButton contoured title='Ver repositório no GitHub' href='#' />
+
+                    <LinkButton contoured href='https://play.google.com/store/apps/details?id=com.ajudame.mobile' title='Ver na Play Store' />
+                    {/* <LinkButton title='Ver repositório no GitHub' href='#' /> */}
                 </div>
             </div>
 
